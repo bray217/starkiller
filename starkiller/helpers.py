09dcf8c17bff9aed37a5be5e7feb3d95f92f65f2
ble61 <ble61@uclive.ac.nz>
2025-10-10 12:28:29 +1300

    still trying to fix merge conflit

--- starkiller/helpers.py
+++ starkiller/helpers.py
@@ -287,7 +287,7 @@ def get_star_cuts(x_length,y_length,image,cat,norm=False):
         star_cuts += [c]
         nancheck = np.sum(np.isnan(c))
         pixs = c.shape[0] * c.shape[1]
-        if ((mx > 5) & (mx < c.shape[1]-5) & (my > 5) & (my < c.shape[0]-5)).any() & (nancheck/pixs<0.2):
+        if ((mx > 2) & (mx < c.shape[1]-2) & (my > 2) & (my < c.shape[0]-2)).any() & (nancheck/pixs<0.2):
             good += [True]
         else:
             good += [False]
@@ -1390,6 +1390,57 @@ def make_false_colour(cube, savePath:str, saveName:str, scaleNorm:str="linear"):
     )
     colour = rgb.plot()
     return colour
+
+
+# Functions for PS1
+
+def isolate_stars(cat,only_stars=False,Qf_lim=0.85,psfkron_diff=0.05):
+    qf_ind = ((cat.gQfPerfect.values > Qf_lim) & (cat.rQfPerfect.values > Qf_lim) & 
+              (cat.iQfPerfect.values > Qf_lim) & (cat.zQfPerfect.values > Qf_lim))
+    kron_ind = (cat.rMeanPSFMag.values - cat.rMeanKronMag.values) < psfkron_diff
+    ind = qf_ind & kron_ind
+    if only_stars:
+        cat = cat.iloc[ind]
+        cat.loc[:,'star'] = 1
+    else:
+        cat.loc[:,'star'] = 0
+        cat.loc[ind,'star'] = 1
+    return cat 
+
+def cut_bad_detections(cat):
+    ind = (cat.rMeanPSFMag.values > 0) & (cat.iMeanPSFMag.values > 0) & (cat.zMeanPSFMag.values > 0)
+    return cat.iloc[ind]
+
+def query_ps1(ra,dec,radius=5/60,only_stars=False,ref_filt='r',maglim=25,catversion='dr2'):
+    '''
+    radius is in degrees !!
+    '''
+    if (catversion.lower() != 'dr2') & (catversion.lower() != 'dr1'):
+        m = 'Version must be dr2, or dr1'
+        raise ValueError(m)
+    coords = f'https://catalogs.mast.stsci.edu/api/v0.1/panstarrs/{catversion.lower()}/mean?ra={ra}&dec={dec}&radius={radius}'
+    conditions = f'&nDetections.gte=5&{ref_filt}MeanPSFMag.gte=0&{ref_filt}MeanPSFMag.lte={maglim}&pagesize=-1&format=csv'
+    html = coords + conditions
+    try:
+        cat = pd.read_csv(html)
+        cat['ra'] = cat['raMean']
+        cat['dec'] = cat['decMean']
+        cat['r'] = cat['rMeanPSFMag']
+        cat['i'] = cat['iMeanPSFMag']
+        cat['g'] = cat['gMeanPSFMag']
+        cat['z'] = cat['zMeanPSFMag']
+        cat['y'] = cat['yMeanPSFMag']
+    except pd.errors.EmptyDataError:
+        print('No detections')
+        cat = []
+    cat = isolate_stars(cat,only_stars=only_stars)
+    cat = cut_bad_detections(cat)
+    cat = cat.sort_values(ref_filt)
+
+    return cat 
+
+
+
 
 def make_false_colour_Sloan(cube, lambdas, savePath:str, saveName:str, scaleNorm:str="linear"):
     """
